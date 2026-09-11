dfed8029f69a6c106e56fb4bbdcf076368da7a04
rafinhaantony <antonyrafinha13@gmail.com>
2026-09-11 15:21:09 -0300

    somativa

--- 2TERMO/BACKEND/SOMATIVA01/EXERCICIO01/ex01_producao.js
+++ 2TERMO/BACKEND/SOMATIVA01/EXERCICIO01/ex01_producao.js
@@ -2,8 +2,8 @@ console.log("------------------------------")
 console.log("      PRODUCAO DO TURNO       ")
 console.log("------------------------------")
 
-const pecasPorHora = 120
-const horasTurno = 8
+const pecasPorHora = entrada.question("Quantas pecas: ");
+const horasTurno = entrada.questionInt("Horas do Turno: ");
 
 const producaoTotal = pecasPorHora * horasTurno
 
